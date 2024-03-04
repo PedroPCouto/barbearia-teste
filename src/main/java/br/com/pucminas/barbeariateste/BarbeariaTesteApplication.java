bb46c9ffd7cadc6a0720c4da1efcf007cf91a24e
&Pedro <pedrohpaiva1258@gmail.com>
2024-03-04 19:13:25 -0300

    teste

---
+++ src/main/java/br/com/pucminas/barbeariateste/BarbeariaTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.pucminas.barbeariateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbeariaTesteApplication.class, args);
+    }
+
+}
